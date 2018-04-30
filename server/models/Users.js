c4349822bba4f7066eebac7921ca8358c23566a4
Ian Miranda <iaansanity@gmail.com>
2018-04-30 17:01:15 +0800

    'Remove'

--- server/models/Users.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-var Users = mongoose.model('Users', new Schema({
-    name:  String
-  }, {collection: 'Users'})
-);
-
-module.exports = Users
